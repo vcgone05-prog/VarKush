7378b6f73f4302b47f30a450cb06a9b96165e01b
Varun Chandra <vrooon@Sambasivas-MacBook-Air.local>
2026-08-03 02:23:13 +0530

    Varshgotherkush.in domain and final date and # addition

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,25 @@ import RSVP from "../components/RSVP";
 
 export default function Home() {
   return (
-    <>
-      <Hero />
-      <LiveLink />
-      <RSVP />
-    </>
+   <>
+  <Hero />
+  <LiveLink />
+  <RSVP />
+
+  <footer className="py-20 text-center">
+    <div className="w-24 h-px bg-[#C89A2A] mx-auto mb-8"></div>
+
+    <p
+      className="text-4xl text-[#7B1D2A]"
+      style={{ fontFamily: "Cormorant Garamond" }}
+    >
+      #VarshGotHerKush
+    </p>
+
+    <p className="mt-4 uppercase tracking-[0.35em] text-lg text-[#A97555]">
+      08.30.2026
+    </p>
+  </footer>
+</>
   );
 }
