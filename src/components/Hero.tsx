d74c374f8f314b2e9b949ecaf68eecec721c52d8
Varun Chandra <vrooon@Sambasivas-MacBook-Air.local>
2026-08-05 02:46:11 +0530

    Favicon final and couple photo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 import toranam from "../assets/images/toranam.webp";
 import ganesha from "../assets/images/ganesha.png";
-import couplePhoto from "../assets/images/newphoto.jpg";
+import couplePhoto from "../assets/images/IMG_7787.jpg";
 import banana from "../assets/images/banana.png";
 import { MapPin } from "lucide-react";
 
